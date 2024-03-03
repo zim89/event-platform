6bfbddc6cdc8f8f58269e64b72bf8896d92fcd27
zim89 <121241841+zim89@users.noreply.github.com>
2024-03-03 15:44:15 +0200

    Update user.model.ts

--- src/lib/mongodb/models/user.model.ts
+++ src/lib/mongodb/models/user.model.ts
@@ -3,9 +3,9 @@ import { Schema, model, models } from 'mongoose';
 const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, default: '' },
-  firstName: { type: String, required: true, default: '' },
-  lastName: { type: String, required: true, default: '' },
+  username: { type: String, default: '' },
+  firstName: { type: String, default: '' },
+  lastName: { type: String, default: '' },
   photo: { type: String, required: true },
 });
 
